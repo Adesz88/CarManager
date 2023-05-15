062976db6ecb0f5e3138dfd4aeffa712a474736c
Adam Olej <51245883+Adesz88@users.noreply.github.com>
2023-05-15 22:57:06 +0200

    Added entry dialog and table view

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,37 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "entrydialog.h"
+#include "string.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    class User {
+    public:
+        unsigned age;
+        std::string name;
+
+        User() {
+            name = "somebody";
+            age = 20;
+        }
+    };
+
     ui->setupUi(this);
+    setCentralWidget(ui -> tableWidget);
+    User somebody;
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_actionAdd_entry_triggered()
+{
+    EntryDialog dialog;
+    dialog.exec();
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,9 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionAdd_entry_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
